e590a427ebf797c044479d64923158fe7556db06
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2019-10-14 01:24:24 +0300

    feat: implement JS-edu-task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+module.exports = function getTimeForEducation(focus = 'family', knowsProgramming = true, config = { family: 4 }) {
+  const middleJS = 800;
+  const basicJS = 500;
+  let hours = 0;
+
+  if (knowsProgramming) {
+    hours = Math.ceil(middleJS / config[focus]);
+  } else {
+    hours = Math.ceil((basicJS + middleJS) / config[focus]);
+  }
+
+  return hours;
+};
